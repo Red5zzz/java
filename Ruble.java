df71007de33321bc0f3d4477608a051b85d724e7
Red5zzz <zhdanok.nika@mail.ru>
2019-04-16 13:44:38 +0300

    add new solution

--- Ruble.java
+++ Ruble.java
@@ -1,54 +1,26 @@
+import java.util.*;
 public class Ruble {
     public static void main(String[] args) {
-        int rubl = 5000;
+        Scanner s = new Scanner(System.in);
+        int rubl = s.nextInt();
         if (rubl % 10 == 0) {
             System.out.print(rubl + " рублей");
         } else {
             System.out.print("");
         }
-        if (rubl % 10 == 1) {
-            System.out.print(rubl + " рубль");
-        } else {
-            System.out.print("");
-        }
-        if (rubl % 10 == 2) {
-            System.out.print(rubl + " рубля");
-        } else {
-            System.out.print("");
-        }
-        if (rubl % 10 == 3) {
-            System.out.print(rubl + " рубля");
-        } else {
-            System.out.print("");
-        }
-        if (rubl % 10 == 4) {
-            System.out.print(rubl + " рубля");
-        } else {
-            System.out.print("");
-        }
-        if (rubl % 10 == 5) {
+        if (11 <= rubl && rubl <= 19) {
             System.out.print(rubl + " рублей");
         } else {
             System.out.print("");
-        }
-        if (rubl % 10 == 6) {
-            System.out.print(rubl + " рублей");
-        } else {
-            System.out.print("");
-            if (rubl % 10 == 7) {
+            if (2 <= rubl % 10 && rubl % 10 <= 4) {
+                System.out.print(rubl + " рубля");
+            } else {
+                System.out.print("");
+            }
+            if (5 <= rubl % 10 && rubl % 10 <= 9) {
                 System.out.print(rubl + " рублей");
             } else {
                 System.out.print("");
-                if (rubl % 10 == 8) {
-                    System.out.print(rubl + " рублей");
-                } else {
-                    System.out.print("");
-                    if (rubl % 10 == 9) {
-                        System.out.print(rubl + " рублей");
-                    } else {
-                        System.out.print("");
-                    }
-                }
             }
         }
     }
