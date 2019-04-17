440c140dc2a3cc4179b3717cbc3fd691c7beff72
Red5zzz <zhdanok.nika@mail.ru>
2019-04-17 19:49:58 +0300

    finished

--- Ruble.java
+++ Ruble.java
@@ -1,15 +1,12 @@
 import java.util.*;
-
 public class Ruble {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Введите число ");
         int rubl = s.nextInt();
-
-        if (rubl % 10 == 1 && rubl % 100 != 11){
-            System.out.print(rubl + " рубль");}
-        else
-        if (2 <= rubl % 10 && rubl % 10 <= 4) {
+        if (rubl % 10 == 1 && rubl % 100 != 11) {
+            System.out.print(rubl + " рубль");
+        } else if (2 <= rubl % 10 && rubl % 10 <= 4 && rubl % 100 != 12 && rubl % 100 != 13 && rubl % 100 != 14) {
             System.out.print(rubl + " рубля");
         } else {
             System.out.print(rubl + " рублей");
